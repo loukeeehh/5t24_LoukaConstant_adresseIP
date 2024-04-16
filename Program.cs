2aeba605b834a9e5c068126ac56936db21c02a72
loukeeehh <louka.constant@gmail.com>
2024-04-16 19:40:00 +0200

    Mon c# adresse IP

---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿namespace _5T24_LoukaConstant_adresseIP
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Tableau de noms et d'adresses IP");
+            int nUser;
+            int[] adresseIP;
+            string[] tableauNoms;
+            int[,] MatriceAdresses;
+            int maxAdresse = 20; 
+            int addAdresse = 0; //compteur pour adresses ajoutées 
+            int addNoms = 0; //compteur pour noms ajoutés
+            string message = "";
+            string keyWord = "stop";
+            string repeatProg;
+            fctProcedure morceauxProg = new fctProcedure();
+
+            do
+            {
+
+                Console.WriteLine("Voulez-vous recommencer ? 'o' = oui, 'n' = non");
+                repeatProg = Console.ReadLine();
+
+
+            } while (repeatProg == "o");
+        }
+    }
+}
---
+++ morceauxProg.cs
@@ -0,0 +1,123 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _5T24_LoukaConstant_adresseIP
+{
+    public struct fctProcedure
+    {
+        public void LireAdresseIP(int[] adresseIP)
+        {
+            int i;
+            int octetAdresse = 4;
+            
+            for (i = 0; i <= octetAdresse - 1; i++)
+            {
+               LireOctet(out adresseIP[i]);
+            }
+        }
+
+        public bool LireOctet(out int nUser)
+        {
+            bool check = false;
+            nUser = 0;
+            while (!check)
+            {
+                Console.WriteLine("Veuillez entrer un nombre entre 0 et 255");
+                if (int.TryParse(Console.ReadLine(), out nUser) && nUser >= 0 && nUser <= 255)
+                {
+                    check = true;
+                }
+
+                else
+                {
+                    Console.WriteLine("Votre nombre n'est pas compris entre 0 et 255 ! Recommencez !");
+                }
+            }
+
+            return check;
+        }
+
+        public bool ajouteAdresseIP(ref int[,] matriceAdresse, ref int[] adresseIP,ref int place)
+        {
+            place = 20;
+           bool addAdresseIP = true;
+            int i;
+
+            if (place <= 20)
+            {
+                for (i = 0; i <= 3; i++)
+                {
+                    matriceAdresse[place, i] = adresseIP[i];
+                }
+            }
+
+            else
+            {
+                addAdresseIP = false;
+            }
+
+            return addAdresseIP;
+        }
+
+        public bool ajouteNom(ref int place, string[] tableauNoms)
+        {  
+            bool addNom = false;
+
+            if (place != tableauNoms.GetLength(place))
+            {
+                tableauNoms[place]= lireString("Quelle est le nom que vous voulez entrer ?");
+                addNom = true; 
+            }
+            return addNom;
+        }
+
+        /*
+        public string concateneAdresse(int[] adresseIP)
+        {
+            string message = "";
+            int i;
+
+            for (i = 0; i <= adresseIP.Length - 1; i++)
+            {
+                message += adresseIP[i];
+
+                if (i <= adresseIP.Length - 1)
+                {
+                    message += ".";
+                }
+                return message;
+            }
+        }
+        */
+        /*
+        public void concateneTout(string[] tableauNoms)
+        {
+            int i;
+            string message = "";
+            int addAdresse = 0;
+            for (i = 0; i < addAdresse ; i++)
+            {
+                message = tableauNoms[i] + concateneAdresse + "\n";
+            }
+            return message;
+        }
+        */
+        public string lireString(string question)
+        {
+            string rep;
+            Console.WriteLine(question);
+            rep = Console.ReadLine();
+            while (rep == "")
+            {
+                Console.WriteLine("La réponse envoyée n'est pas bonnne, veuillez répéter la question !");
+                Console.WriteLine(question);
+                rep = Console.ReadLine();
+            }
+            return rep;
+        }
+
+    }
+}
